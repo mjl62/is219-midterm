983703604c3731b262e699519fc1d49893194bc9
mjl62 <mjl62@njit.edu>
2024-03-28 17:16:57 -0400

    Added testing implementation of plugins
    Subject to change in near future

---
+++ app/plugins/exit/__init__.py
@@ -0,0 +1,12 @@
+""" Command for exiting a program through the CLI """
+import sys
+from app import Command
+
+class Exit(Command):
+    """ Adds 'exit' command to stop a running program """
+    def __init__(self):
+        super().__init__(command_string="exit", help_string="Exits the program")
+
+    def run(self, args:list=None):
+        """ Exit the program when the command is executed """
+        sys.exit(0)
